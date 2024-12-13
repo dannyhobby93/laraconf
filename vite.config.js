357fba94f5ab3d738c9bdedf8f9a69346771ec9f
Danny Hobby <danny.hobby@outlook.com>
2024-12-13 15:23:29 +0000

    Installation

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import laravel, { refreshPaths } from "laravel-vite-plugin";
-
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: [...refreshPaths, "App/Filament/**", "App/Livewire/**"],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: [
+                ...refreshPaths,
+                'app/Filament/**',
+                'app/Forms/Components/**',
+                'app/Livewire/**',
+                'app/Infolists/Components/**',
+                'app/Providers/Filament/**',
+                'app/Tables/Columns/**',
+            ],
         }),
     ],
-});
+})
